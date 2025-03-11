47560891937fc07c4695c07230ab33916b1cd6f2
Sherman Walter <srzprivate@outlook.com>
2025-03-11 18:16:46 -0300

    adicionado método para exibir as informações do livro

--- src/main/java/com/desbravando_java_poo/Livro.java
+++ src/main/java/com/desbravando_java_poo/Livro.java
@@ -5,4 +5,16 @@ public class Livro {
     String descricao;
     double preco;
     String isbn;
+
+    void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do livro");
+        System.out.printf(
+                "Nome: %s\n" +
+                        "Descrição: %s\n" +
+                        "Preço: %.2f\n" +
+                        "ISBN: %s\n" +
+                        "--\n",
+                nome, descricao, preco, isbn);
+                
+    }
 }
--- src/main/java/com/desbravando_java_poo/Main.java
+++ src/main/java/com/desbravando_java_poo/Main.java
@@ -2,6 +2,12 @@ package com.desbravando_java_poo;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Livro livro = new Livro();
+        livro.nome = "Java 8 Prático";
+        livro.descricao = "Novos recursos da linguagem";
+        livro.preco = 59.99;
+        livro.isbn = "978";
+
+        livro.mostrarDetalhes();
     }
 }
