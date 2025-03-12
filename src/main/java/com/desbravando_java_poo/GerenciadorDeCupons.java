1f5b6cacd4a0b723f4677b868b0b0c06afd46f2b
Sherman Walter <srzprivate@outlook.com>
2025-03-12 13:31:55 -0300

    criada nova classe para gerenciar cupons de desconto

---
+++ src/main/java/com/desbravando_java_poo/GerenciadorDeCupons.java
@@ -0,0 +1,12 @@
+package com.desbravando_java_poo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GerenciadorDeCupons {
+    private List<String> cupons;
+
+    public GerenciadorDeCupons() {
+        this.cupons = Arrays.asList("SHER081", "99CUPONS", "HAPPY2025");
+    }
+}
