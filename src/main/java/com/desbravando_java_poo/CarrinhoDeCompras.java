747450c61f2350f0a9ada07399c0c0ad2b9ac49c
Sherman Walter <srzprivate@outlook.com>
2025-03-12 09:47:00 -0300

    adicionada classe CarrinhoDeCompras

---
+++ src/main/java/com/desbravando_java_poo/CarrinhoDeCompras.java
@@ -0,0 +1,7 @@
+package com.desbravando_java_poo;
+
+public class CarrinhoDeCompras {
+    public void adiciona(Livro livro) {
+        System.out.println("Adicionado: " + livro);
+    }
+}
--- src/main/java/com/desbravando_java_poo/Main.java
+++ src/main/java/com/desbravando_java_poo/Main.java
@@ -3,25 +3,20 @@ package com.desbravando_java_poo;
 public class Main {
     public static void main(String[] args) {
 
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
         Autor autor = new Autor();
         autor.setNome("Sherman Walter");
 
-        Livro livro = new Livro(autor);
-        livro.setPreco(59.90);
-
-        if (!livro.aplicarDesconto(0.3)) {
-            System.out.println("Desconto no livro não pode ser maior do que 30%.\n");
-        } else {
-            System.out.printf("Valor no livro com desconto: R$%.2f\n", livro.getPreco());
-        }
-
+        LivroFisico fisico = new LivroFisico(autor);
+        fisico.setNome("Test-Driven Development");
+        fisico.setPreco(59.90);
+        
         Ebook ebook = new Ebook(autor);
+        ebook.setNome("Test-Driven Development Ebook");
         ebook.setPreco(29.90);
 
-        if (!ebook.aplicarDesconto(0.15)) {
-            System.out.println("Desconto no ebook não pode ser maior do que 15%.\n");
-        } else {
-            System.out.printf("Valor no ebook com desconto: R$%.2f\n", ebook.getPreco());
-        }
+        carrinho.adiciona(ebook);
+        carrinho.adiciona(fisico);
     }
 };
